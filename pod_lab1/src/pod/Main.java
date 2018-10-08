35c6845631852d243b4b95bae2106259a7314506
pako911 <pako911@onet.pl>
2018-10-08 22:26:36 +0200

    1st update

--- pod_lab1/src/pod/Main.java
+++ pod_lab1/src/pod/Main.java
@@ -1,19 +1,36 @@
 package pod;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	    String key = "vigenere";
-	    String text = "algorytmy i struktury danych";
+        String key = "vigenere";
+        String text = "Algorytmy i Struktury Danych";
 
         encrypt(key, text);
     }
     static void encrypt(String key, String text){
-        if(key.length()<text.length()){
-            String passphase;
-            for(int i = 0; i<text.length(); i++){
-                passphase[i] = 
-            }
+        text = text.replaceAll("\\s", "").toLowerCase();
+        List<Character> cipher = new ArrayList<Character>(text.length());
+        List<Character> passphase = new ArrayList<Character>(text.length());
+
+        for(int i = 0, n = 0; i < text.length(); i++, n++){
+            if(n == key.length()) n = 0;
+            passphase.add(key.charAt(n));
         }
+        cipher.add(passphase.get(0));
+        for(int i = 1, shift, newChar; i < text.length(); i++){
+            shift = passphase.get(i)-97;
+            newChar = (int)text.charAt(i)+shift;
+            if(newChar > 122) newChar = 96+newChar-122;
+            cipher.add((char)newChar);
+            //System.out.println(text.charAt(i) + "->" + cipher.get(i) + "(" + (int)cipher.get(i) + ") ");
+        }
+
+        System.out.println(cipher);
+        //System.out.println(passphase);
     }
 }
