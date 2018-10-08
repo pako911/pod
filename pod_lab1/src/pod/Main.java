c22dd5fd016c6c37938ecb24af12ae38df82488d
pako911 <pako911@onet.pl>
2018-10-08 20:38:48 +0200

    1st  commit

---
+++ pod_lab1/src/pod/Main.java
@@ -0,0 +1,19 @@
+package pod;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String key = "vigenere";
+	    String text = "algorytmy i struktury danych";
+
+        encrypt(key, text);
+    }
+    static void encrypt(String key, String text){
+        if(key.length()<text.length()){
+            String passphase;
+            for(int i = 0; i<text.length(); i++){
+                passphase[i] = 
+            }
+        }
+    }
+}
